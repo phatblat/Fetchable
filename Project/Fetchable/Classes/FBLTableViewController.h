3c6de55092cb41cfb01bf186b06349f5e26592d2
Ben Chatelain <benchatelain@gmail.com>
2014-02-19 22:24:24 -0700

    Add FBLTableViewController stub

---
+++ Project/Fetchable/Classes/FBLTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FBLTableViewController.h
+//  Fetchable
+//
+//  Created by Ben Chatelain on 2/19/14.
+//
+//
+
+#import "FBLFetchedResultsTableViewController.h"
+
+@interface FBLTableViewController : FBLFetchedResultsTableViewController
+
+@end
---
+++ Project/Fetchable/Classes/FBLTableViewController.m
@@ -0,0 +1,41 @@
+//
+//  FBLTableViewController.m
+//  Fetchable
+//
+//  Created by Ben Chatelain on 2/19/14.
+//
+//
+
+#import "FBLTableViewController.h"
+
+@interface FBLTableViewController ()
+
+@end
+
+@implementation FBLTableViewController
+
+- (void)viewDidLoad
+{
+//    ConfigureCellBlock = ^(UITableViewCell *tableCell, id item) {
+//        Equipment *equipment = (Equipment *)item;
+//        FFEquipmentCell *cell = (FFEquipmentCell *)tableCell;
+//        cell.thingNameLabel.text = equipment.name;
+//        cell.roomNameLabel.text = equipment.room.name;
+//
+//        if ([weakSelf.currentRoom isEqual:equipment.room]) {
+//            cell.currentRoomIndicatorView.hidden = NO;
+//        } else {
+//            cell.currentRoomIndicatorView.hidden = YES;
+//        }
+//
+//        if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+//            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+//            // Reduce the right margin since the disclosure indicator adds 10pt
+//            cell.roomNameRightHorizontalConstraint.constant = 5;
+//        } else {
+//            cell.accessoryType = UITableViewCellAccessoryNone;
+//        }
+//    };
+}
+
+@end
