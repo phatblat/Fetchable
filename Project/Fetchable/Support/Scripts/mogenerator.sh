e925fa3c4c5b49f3a4dfccaa520761687fee4695
Ben Chatelain <benchatelain@gmail.com>
2014-02-19 22:45:11 -0700

    Add mogenerator script, build phase and initial data model

---
+++ Project/Fetchable/Support/Scripts/mogenerator.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#  mogenerator.sh
+#  Fetchable
+#
+#  Created by Ben Chatelain on 2/19/14.
+#  Copyright (c) 2014 @phatblat. All rights reserved.
+#
+
+PATH=${PATH}:/usr/local/bin:/opt/boxen/homebrew/bin
+mogenerator \
+    --model "Fetchable/Model/Fetchable.xcdatamodeld" \
+    --human-dir "Fetchable/Model/Managed Objects/" \
+    --machine-dir "Fetchable/Model/Managed Objects/_Generated" \
+    --template-var \
+    arc=true
