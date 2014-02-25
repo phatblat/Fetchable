724833dc672901d6e1ec4773d0886aaed01b2881
Ben Chatelain <benchatelain@gmail.com>
2014-02-25 07:42:58 -0700

    Stand up core data stack and insert a book on launch

--- Project/Fetchable/AppDelegate.m
+++ Project/Fetchable/AppDelegate.m
@@ -7,7 +7,45 @@
 //
 
 #import "AppDelegate.h"
+#import "Book.h"
+#import <CoreData+MagicalRecord.h>
 
 @implementation AppDelegate
+
+#pragma mark - UIApplicationDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    [self setupData];
+
+    return YES;
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    [MagicalRecord cleanUp];
+}
+
+#pragma mark - Data
+
+- (void)setupData
+{
+    [MagicalRecord setupCoreDataStack];
+
+    Book *book;
+//    book = [Book MR_importFromObject:@{ @"title": @"Core Data: Data Storage and Management for iOS, OS X, and iCloud",
+//                                        @"asin": @"B00DZ48BM2",
+//                                        @"fileSize": @"1516",
+//                                        @"fileSizeUnits": @"KB",
+//                                        @"pageCount": @"256",
+//                                        @"price": @"28.51",
+//                                        @"publishDate": NSNull,
+//                                      }];
+
+    book = [Book MR_createEntity];
+    book.title = @"Core Data";
+
+    [[NSManagedObjectContext MR_contextForCurrentThread] MR_saveToPersistentStoreWithCompletion:nil];
+}
 
 @end
