a49adf416b8fb4ffe178e57139335531d2eba42a
Ben Chatelain <benchatelain@gmail.com>
2014-02-25 07:43:18 -0700

    Configure initial table view

--- Project/Fetchable/Classes/FBLTableViewController.m
+++ Project/Fetchable/Classes/FBLTableViewController.m
@@ -7,35 +7,28 @@
 //
 
 #import "FBLTableViewController.h"
-
-@interface FBLTableViewController ()
-
-@end
+#import "Book.h"
+#import <FBLFetchedResultsTableViewDataSource.h>
+#import <CoreData+MagicalRecord.h>
 
 @implementation FBLTableViewController
 
 - (void)viewDidLoad
 {
-//    ConfigureCellBlock = ^(UITableViewCell *tableCell, id item) {
-//        Equipment *equipment = (Equipment *)item;
-//        FFEquipmentCell *cell = (FFEquipmentCell *)tableCell;
-//        cell.thingNameLabel.text = equipment.name;
-//        cell.roomNameLabel.text = equipment.room.name;
-//
-//        if ([weakSelf.currentRoom isEqual:equipment.room]) {
-//            cell.currentRoomIndicatorView.hidden = NO;
-//        } else {
-//            cell.currentRoomIndicatorView.hidden = YES;
-//        }
-//
-//        if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-//            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-//            // Reduce the right margin since the disclosure indicator adds 10pt
-//            cell.roomNameRightHorizontalConstraint.constant = 5;
-//        } else {
-//            cell.accessoryType = UITableViewCellAccessoryNone;
-//        }
-//    };
+    [super viewDidLoad];
+
+    ConfigureCellBlock configureCellBlock = ^(UITableViewCell *tableCell, id item) {
+        Book *book = (Book *)item;
+        tableCell.textLabel.text = book.title;
+    };
+
+    NSFetchedResultsController *frc = [Book MR_fetchAllSortedBy:@"title" ascending:YES
+                                                  withPredicate:nil
+                                                        groupBy:nil delegate:self];
+
+    self.dataSource = [[FBLFetchedResultsTableViewDataSource alloc] initWithFetchedResultsController:frc
+                                                                                      cellIdentifier:@"BookCell"
+                                                                                  configureCellBlock:configureCellBlock];
 }
 
 @end
