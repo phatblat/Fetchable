c46c9a7e9140634a23b8aab172f6df1c582fddf5
Ben Chatelain <benchatelain@gmail.com>
2014-03-05 06:24:26 -0700

    Switch to using MR_importFromObject to stand up initial data

--- Project/Fetchable/Classes/FBLDataManager.m
+++ Project/Fetchable/Classes/FBLDataManager.m
@@ -51,17 +51,17 @@
 + (void)importInitialData
 {
     Book *book;
-    //    book = [Book MR_importFromObject:@{ @"title": @"Core Data: Data Storage and Management for iOS, OS X, and iCloud",
-    //                                        @"asin": @"B00DZ48BM2",
-    //                                        @"fileSize": @"1516",
-    //                                        @"fileSizeUnits": @"KB",
-    //                                        @"pageCount": @"256",
-    //                                        @"price": @"28.51",
-    //                                        @"publishDate": NSNull,
-    //                                      }];
+    book = [Book MR_importFromObject:@{ @"title": @"Core Data: Data Storage and Management for iOS, OS X, and iCloud",
+                                        @"asin": @"B00DZ48BM2",
+                                        @"fileSize": @"1516",
+                                        @"fileSizeUnits": @"KB",
+                                        @"pageCount": @"256",
+                                        @"price": @"28.51",
+//                                        @"publishDate": NSNull,
+                                      }];
 
-    book = [Book MR_createEntity];
-    book.title = @"Core Data";
+//    book = [Book MR_createEntity];
+//    book.title = @"Core Data";
 }
 
 @end
